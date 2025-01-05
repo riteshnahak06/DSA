897d0547abd2acd286e653149e37beb6806b0cd9
nahakritesh06 <nahakritesh06@gmail.com>
2025-01-05 20:23:18 +0530

    Heap

---
+++ src/main/java/com/dsa/heap/C01MinHeap.java
@@ -0,0 +1,13 @@
+package src.main.java.com.dsa.heap;
+
+import java.util.PriorityQueue;
+
+public class C01MinHeap {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.add(7);
+        minHeap.add(1);
+        minHeap.add(22);
+        minHeap.add(33);
+    }
+}
