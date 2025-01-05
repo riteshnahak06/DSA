c7f629ec93d3e8dfbe983ae3729c30cca5e6a5f7
nahakritesh06 <nahakritesh06@gmail.com>
2025-01-05 20:23:56 +0530

    Closest element

---
+++ src/main/java/com/dsa/heap/P03KClosestElement.java
@@ -0,0 +1,16 @@
+package src.main.java.com.dsa.heap;
+/*
+{10,15,7,3,4}, target =8,k=2 --> 2  element which is nearer  to target is 7,10
+ */
+
+public class P03KClosestElement {
+    public static void main(String[] args) {
+        int [] arr={10,15,7,3,4};
+        kClosestElement(arr,8,2);
+    }
+
+    private static void kClosestElement(int[] arr, int target, int k) {
+
+    }
+
+}
