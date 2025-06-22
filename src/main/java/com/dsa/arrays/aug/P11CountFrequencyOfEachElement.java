cb247f6d62d6e960ce0ec8c466ca153e67111e70
nahakritesh06 <nahakritesh06@gmail.com>
2025-06-22 19:21:45 +0530

    MaximumSumSubArray

--- src/main/java/com/dsa/arrays/aug/P11CountFrequencyOfEachElement.java
+++ src/main/java/com/dsa/arrays/aug/P11CountFrequencyOfEachElement.java
@@ -1,7 +1,23 @@
 package src.main.java.com.dsa.arrays.aug;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P11CountFrequencyOfEachElement {
     public static void main(String[] args) {
         int [] arr={1,2,2,3,3,4};
+        countFrequency(arr);
+    }
+
+    private static void countFrequency(int[] arr) {
+        Map<Integer,Integer> hm=new HashMap<>();
+        for (int i=0;i< arr.length;i++){
+            if (hm.containsKey(arr[i])){
+                hm.put(arr[i],hm.get(arr[i])+1);
+            }else {
+                hm.put(arr[i],1);
+            }
+        }
+        System.out.println(hm);
     }
 }
